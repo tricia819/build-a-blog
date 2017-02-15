e4ddd3bfa623f9bb89ea635ddc0760c914b53bdd
Tricia Gray <tricia819@gmail.com>
2017-02-14 20:02:50 -0600

    First page of homework done.

--- main.py
+++ main.py
@@ -15,11 +15,73 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment (loader = jinja2.FileSystemLoader (template_dir),
+                               autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+  def write(self, *a, **kw):
+    self.response.out.write(*a, **kw)
+
+  def render_str (self, template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+  def render (self, template, **kw):
+    self.write(self.render_str(template, **kw))
+
+class Entry(db.Model):  #creates an entity
+  title = db.StringProperty(required = True) #constraint, won't let you NOT enter a title
+  entry = db.TextProperty(required = True)
+  created = db.DateTimeProperty(auto_now_add = True) #set created at current time when entry is created
+  #all this in google docs for datastore
+
+class Blog(Handler):
+  def render_blog(self, title="", entry=""):
+
+    entrys = db.GqlQuery("SELECT * FROM Entry \
+                        ORDER BY created DESC LIMIT 5")
+
+    self.render("blog.html", title= title, entry= entry, entrys=entrys)
+
+  def get (self):
+    self.render_blog()
+
+  def post (self):
+    title = self.request.get("title")
+    entry = self.request.get("entry")
+
+
+class NewPost(Handler):
+  def render_new_post(self, title="", entry="", error=""):
+    self.render("new-post.html", title=title, entry=entry, error=error)
+
+  def get (self):
+    self.render_new_post()
+
+  def post (self):
+
+    title = self.request.get("title")
+    entry = self.request.get("entry")
+
+    if title and entry:
+      e = Entry(title = title, entry = entry)
+      #creates new instance of entry
+      e.put()
+      #stores new entry object in database
+      self.redirect("/blog" )
+      
+    else:
+      error = "You must enter BOTH a title AND a blog post. Please try again."
+      self.render_new_post(title, entry, error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+('/', Blog),
+('/blog', Blog),
+('/newpost', NewPost),
+], debug =True)
